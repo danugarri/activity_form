e40429163c87c7b77c7fa7abe440a903f22f4ce8
daniel <danugarri@gmail.com>
2021-10-07 15:11:45 +0200

    solving input problems

--- inputController.php
+++ inputController.php
@@ -7,21 +7,8 @@
         //changing the activity box to visible
         echo '<script type="text/javascript" src="main.js"></script>';
         //Controlling user input
-        //if radio button is checked
-          if(isset($_POST['access'])){//without isset() it will return a warning
-            //var_dump('opcion1');
-          $free='<h3>Gratuita</h3>';
-          /*creating an instance for the class Activity declared in activity.php*/
-          $add_activity= new Activity($_POST['title'],$_POST['date'],$_POST['city'],$_POST['type'],$free);
-          //strtolower() method : to transform a string into lower cases
-          $activity_image= strtolower($_POST['type']).'.jpg';//this recibe the 'type' name from <option>
-          
-          //showing on the page
-          echo "<div>$add_activity->title</div><br/><div>$add_activity->date</div><br/>
-          <br/><div>$add_activity->city</div><br/><div>$free</div></section>";
-        }
         //radio button on and option selected
-        else if(isset($_POST['access']) && isset($_POST['type'])){
+         if(isset($_POST['access']) && isset($_POST['type'])){
           //var_dump('opcion2');
           $free='<h3>Gratuita</h3>';
           /*creating an instance for the class Activity declared in activity.php*/
